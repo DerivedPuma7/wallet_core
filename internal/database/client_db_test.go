31207a8b7ab5ed9b00bf8ae1c66fc10454c6b78b
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-07-10 15:30:48 -0300

    test: clientDb tests

---
+++ internal/database/client_db_test.go
@@ -0,0 +1,52 @@
+package database
+
+import (
+	"database/sql"
+	"testing"
+
+	"github.com.br/derivedpuma7/wallet-core/internal/entity"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/stretchr/testify/suite"
+)
+
+type ClientDbTestSuite struct {
+	suite.Suite
+	db *sql.DB
+	clientDb *ClientDb
+}
+
+func (s *ClientDbTestSuite) SetupSuite() {
+	db, err := sql.Open("sqlite3", ":memory")
+	s.Nil(err)
+	s.db = db
+	db.Exec("CREATE TABLE clients (id string, name string, email string, createdAt date, updatedAt date)")
+	s.clientDb = NewClientDb(db)
+}
+
+func(s *ClientDbTestSuite) TearDownSuite() {
+	defer s.db.Close()
+	s.db.Exec("DROP TABLE clients")
+}
+
+func TestClientDbTestSuite(t *testing.T) {
+	suite.Run(t, new(ClientDbTestSuite))
+}
+
+func (s *ClientDbTestSuite) TestSave() {
+	client, _ := entity.NewClient("any name", "any email")
+	err := s.clientDb.Save(client)
+
+	s.Nil(err)
+}
+
+func (s *ClientDbTestSuite) TestGet() {
+	client, _ := entity.NewClient("any name", "any email")
+	s.clientDb.Save(client)
+
+	clientDb, err := s.clientDb.Get(client.ID)
+
+	s.Nil(err)
+	s.Equal(client.ID, clientDb.ID)
+	s.Equal(client.Name, clientDb.Name)
+	s.Equal(client.Email, clientDb.Email)
+}
