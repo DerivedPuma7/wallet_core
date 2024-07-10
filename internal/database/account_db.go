98f6a271b22c5c6dc77d60c0341397dfd1373df5
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-07-10 18:04:05 -0300

    feat: implement AccountDb database gateway

---
+++ internal/database/account_db.go
@@ -0,0 +1,69 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com.br/derivedpuma7/wallet-core/internal/entity"
+)
+
+type AccountDb struct {
+	DB *sql.DB
+}
+
+func NewAccountDb(db *sql.DB) *AccountDb {
+	return &AccountDb{
+		DB: db,
+	}
+}
+
+func (a *AccountDb) FindById(id string) (*entity.Account, error) {
+	var account entity.Account
+	var client entity.Client
+	account.Client = &client
+	stmt, err := a.DB.Prepare(`
+		SELECT 
+			a.id, a.clientId, a.balance, a.createdAt, a.updatedAt,
+			c.id, c.name, c.email, c.createdAt, c.updatedAt
+		FROM accounts as a 
+		INNER JOIN clients as c 
+			ON a.clientId = c.id 
+		WHERE a.id = ?
+	`)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	row := stmt.QueryRow(id)
+	err = row.Scan(
+		&account.ID,
+		&account.Client.ID,
+		&account.Balance,
+		&account.CreatedAt,
+		&account.UpdatedAt,
+		&client.ID,
+		&client.Name,
+		&client.Email,
+		&client.CreatedAt,
+		&client.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &account, nil
+}
+
+func (a *AccountDb) Save(account *entity.Account) error {
+	stmt, err := a.DB.Prepare(`
+		INSERT INTO accounts(id, clientId, balance, createdAt, updatedAt)
+		VALUES(?, ?, ?, ?, ?)
+	`)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(account.ID, account.Client.ID, account.Balance, account.CreatedAt, account.UpdatedAt)
+	if err != nil {
+		return err
+	}
+	return nil
+}
