a611337dcea98fdd91b0335c21aa957ca7163711
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-07-16 11:14:31 -0300

    feat: implement http handlers

---
+++ internal/web/account.handler.go
@@ -0,0 +1,42 @@
+package web
+
+import (
+	"encoding/json"
+	"net/http"
+
+	createaccount "github.com.br/derivedpuma7/wallet-core/internal/usecase/create_account"
+)
+
+type WebAccountHandler struct {
+  CreateAccountUseCase createaccount.CreateAccountUseCase
+}
+
+func NewWebAccountHandler(createAccountUseCase createaccount.CreateAccountUseCase) *WebAccountHandler {
+  return &WebAccountHandler{
+    CreateAccountUseCase: createAccountUseCase,
+  }
+}
+
+func (h *WebAccountHandler) CreateAccount(w http.ResponseWriter, r *http.Request) {
+  var dto createaccount.CreateAccountInputDto
+  err := json.NewDecoder(r.Body).Decode(&dto)
+  if err != nil {
+    w.WriteHeader(http.StatusBadRequest)
+    return
+  }
+  
+  output, err := h.CreateAccountUseCase.Execute(dto)
+  if err != nil {
+    w.WriteHeader(http.StatusInternalServerError)
+    return
+  }
+
+  w.Header().Set("Content-Type", "application/json")
+  err = json.NewEncoder(w).Encode(output)
+  if err != nil {
+    w.WriteHeader(http.StatusInternalServerError)
+    return
+  }
+
+  w.WriteHeader(http.StatusCreated)
+}
---
+++ internal/web/transaction_handler.go
@@ -0,0 +1,42 @@
+package web
+
+import (
+	"encoding/json"
+	"net/http"
+
+	createtransaction "github.com.br/derivedpuma7/wallet-core/internal/usecase/create_transaction"
+)
+
+type WebTransactionHandler struct {
+  CreateTransacitonUseCase createtransaction.CreateTransactionUseCase
+}
+
+func NewWebTransactionHandler(createTransactionUseCase createtransaction.CreateTransactionUseCase) *WebTransactionHandler {
+  return &WebTransactionHandler{
+    CreateTransacitonUseCase: createTransactionUseCase,
+  }
+}
+
+func (h *WebTransactionHandler) CreateTransaction(w http.ResponseWriter, r *http.Request) {
+  var dto createtransaction.CreateTransactionInputDto
+  err := json.NewDecoder(r.Body).Decode(&dto)
+  if err != nil {
+    w.WriteHeader(http.StatusBadRequest)
+    return
+  }
+  
+  output, err := h.CreateTransacitonUseCase.Execute(dto)
+  if err != nil {
+    w.WriteHeader(http.StatusInternalServerError)
+    return
+  }
+
+  w.Header().Set("Content-Type", "application/json")
+  err = json.NewEncoder(w).Encode(output)
+  if err != nil {
+    w.WriteHeader(http.StatusInternalServerError)
+    return
+  }
+
+  w.WriteHeader(http.StatusCreated)
+}
