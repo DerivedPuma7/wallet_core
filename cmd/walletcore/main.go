7ffe45b56208487e8807847917c9827185e06ce1
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-07-17 16:31:31 -0300

    fix: fix connection with mysql and kafka

--- cmd/walletcore/main.go
+++ cmd/walletcore/main.go
@@ -22,14 +22,14 @@ import (
 )
 
 func main() {
-  db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", "root", "root", "localhost", "3306", "wallet"))
+  db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", "root", "root", "mysql", "3306", "wallet"))
   if err != nil {
     panic(err)
   }
   defer db.Close()
 
   configMap := ckafka.ConfigMap{
-    "bootstrap.servers": "kafka:9094",
+    "bootstrap.servers": "kafka:29092",
     "group.id": "wallet",
   }
   kafkaProducer := kafka.NewKafkaProducer(&configMap)
