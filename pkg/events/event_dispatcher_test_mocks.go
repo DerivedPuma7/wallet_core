2c45ac71aa9e5493c47b0a7b002f145afca5d0fc
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-07-17 15:46:00 -0300

    test: adjust mocks

--- pkg/events/event_dispatcher_test_mocks.go
+++ pkg/events/event_dispatcher_test_mocks.go
@@ -1,6 +1,7 @@
 package events
 
 import (
+	"sync"
 	"time"
 
 	"github.com/stretchr/testify/mock"
@@ -32,7 +33,7 @@ type TestEventHandler struct{
   ID int
 }
 
-func (h *TestEventHandler) Handle(event EventInterface) {}
+func (h *TestEventHandler) Handle(event EventInterface, wg *sync.WaitGroup) {}
 
 type EventDispatcherTestSuite struct {
   suite.Suite
@@ -63,6 +64,7 @@ type MockHandler struct {
   mock.Mock
 }
 
-func (m *MockHandler) Handle(event EventInterface) {
+func (m *MockHandler) Handle(event EventInterface, wg *sync.WaitGroup) {
   m.Called(event)
+  wg.Done()
 }
