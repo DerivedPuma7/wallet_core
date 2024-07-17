e9ca8cb457c87212efaead5d8b3edaf71289bd20
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-07-17 09:50:33 -0300

    feat: implement database transactions when creating a new transaction

--- internal/usecase/create_transaction/create_transaction.go
+++ internal/usecase/create_transaction/create_transaction.go
@@ -1,9 +1,12 @@
 package createtransaction
 
 import (
+	"context"
+
 	"github.com.br/derivedpuma7/wallet-core/internal/entity"
 	"github.com.br/derivedpuma7/wallet-core/internal/gateway"
 	"github.com.br/derivedpuma7/wallet-core/pkg/events"
+	"github.com.br/derivedpuma7/wallet-core/pkg/uow"
 )
 
 type CreateTransactionInputDto struct {
@@ -14,57 +17,84 @@ type CreateTransactionInputDto struct {
 
 type CreateClientOutputDto struct {
 	ID string
+  AccountIdFrom string
+  AccountIdTo string
+  Amount float64
 }
 
 type CreateTransactionUseCase struct {
-	TransactionGateway gateway.TransactionGateway
-	AccountGateway gateway.AccountGateway
+  Uow uow.UowInterface
 	EventDispatcher events.EventDispatcherInterface
 	TransactionCreated events.EventInterface
 }
 
 func NewCreateTransactionUseCase(
-	transactionGateway gateway.TransactionGateway, 
-	accountGateway gateway.AccountGateway,
+  uow uow.UowInterface,
 	eventDispatcher events.EventDispatcherInterface,
 	transactionCreated events.EventInterface,
 ) *CreateTransactionUseCase {
 	return &CreateTransactionUseCase{
-		TransactionGateway: transactionGateway, 
-		AccountGateway: accountGateway, 
+    Uow: uow,
 		EventDispatcher: eventDispatcher,
 		TransactionCreated: transactionCreated,
 	}
 }
 
-func (uc *CreateTransactionUseCase) Execute(input CreateTransactionInputDto) (*CreateClientOutputDto, error) {
-	accountFrom, err := uc.AccountGateway.FindById(input.AccountIDFrom)
-	if err != nil {
-		return nil, err
-	}
-	accountTo, err := uc.AccountGateway.FindById(input.AccountIDTo)
-	if err != nil {
-		return nil, err
-	}
-	transaction, err := entity.NewTransaction(accountFrom, accountTo, input.Amount)
-	if err != nil {
-		return nil, err
-	}
-  err = uc.AccountGateway.UpdateBalance(accountFrom)
+func (uc *CreateTransactionUseCase) getAccountRepository(ctx context.Context) gateway.AccountGateway {
+  repo, err := uc.Uow.GetRepository(ctx, "AccountDb")
   if err != nil {
-		return nil, err
-	}
-  err = uc.AccountGateway.UpdateBalance(accountTo)
+    panic(err)
+  }
+  return repo.(gateway.AccountGateway)
+}
+
+func (uc *CreateTransactionUseCase) getTransactionRepository(ctx context.Context) gateway.TransactionGateway {
+  repo, err := uc.Uow.GetRepository(ctx, "TransactionDb")
   if err != nil {
-		return nil, err
-	}
-	err = uc.TransactionGateway.Create(transaction)
-	if err != nil {
-		return nil, err
-	}
-	output := &CreateClientOutputDto{
-		ID: transaction.ID,
-	}
+    panic(err)
+  }
+  return repo.(gateway.TransactionGateway)
+}
+
+func (uc *CreateTransactionUseCase) Execute(ctx context.Context, input CreateTransactionInputDto) (*CreateClientOutputDto, error) {
+  output := &CreateClientOutputDto{}
+  err := uc.Uow.Do(ctx, func(_ *uow.Uow) error {
+    accountRepository := uc.getAccountRepository(ctx)
+    transactionRepository := uc.getTransactionRepository(ctx)
+    
+    accountFrom, err := accountRepository.FindById(input.AccountIDFrom)
+    if err != nil {
+      return err
+    }
+    accountTo, err := accountRepository.FindById(input.AccountIDTo)
+    if err != nil {
+      return err
+    }
+    transaction, err := entity.NewTransaction(accountFrom, accountTo, input.Amount)
+    if err != nil {
+      return err
+    }
+    err = accountRepository.UpdateBalance(accountFrom)
+    if err != nil {
+      return err
+    }
+    err = accountRepository.UpdateBalance(accountTo)
+    if err != nil {
+      return err
+    }
+    err = transactionRepository.Create(transaction)
+    if err != nil {
+      return err
+    }
+    output.ID = transaction.ID
+    output.AccountIdFrom = accountFrom.ID
+    output.AccountIdTo = accountTo.ID
+    output.Amount = transaction.Amount
+    return nil
+  })
+  if err != nil {
+    return nil, err
+  }
 	uc.TransactionCreated.SetPayload(output)
 	uc.EventDispatcher.Dispatch(uc.TransactionCreated)
 	return output, nil
