0f541cd81d0a71307643c50e5575095ae0304bc0
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-07-17 15:41:29 -0300

    feat: implement handler interface with kafka producer

---
+++ internal/event/handler/transaction_created_kafka.go
@@ -0,0 +1,27 @@
+package handler
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com.br/derivedpuma7/wallet-core/pkg/events"
+	"github.com.br/derivedpuma7/wallet-core/pkg/kafka"
+)
+
+type TransactionCreatedKafkaHandler struct {
+	Kafka *kafka.Producer
+}
+
+var _ events.EventHandlerInterface = (*TransactionCreatedKafkaHandler)(nil)
+
+func NewTransactionCreatedKafkaHandler(kafka *kafka.Producer) *TransactionCreatedKafkaHandler {
+	return &TransactionCreatedKafkaHandler{
+		Kafka: kafka,
+	}
+}
+
+func (h *TransactionCreatedKafkaHandler) Handle(event events.EventInterface, wg *sync.WaitGroup) {
+	defer wg.Done()
+  h.Kafka.Publish(event, nil, "transactions")
+  fmt.Println(`TransactionCreatedKafkaHandler:`, event)
+}
